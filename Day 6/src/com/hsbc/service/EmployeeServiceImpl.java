4b874ea6c445f92aef1cfaf9e3d3408ec58c8553
Supraja-p <89636935+Supraja-p@users.noreply.github.com>
2021-10-13 10:13:38 +0530

    Updated

--- Day 6/src/com/hsbc/service/EmployeeServiceImpl.java
+++ Day 6/src/com/hsbc/service/EmployeeServiceImpl.java
@@ -7,7 +7,7 @@ import com.hsbc.exceptions.EmployeeAlreadyExistsException;
 import com.hsbc.exceptions.EmployeeNotFoundException;
 
 public class EmployeeServiceImpl implements EmployeeService {
-	private EmployeeDao dao;
+	private EmployeeDao dao =null;
 	public EmployeeServiceImpl(EmployeeDao dao) {
 		this.dao = dao;
 		}
